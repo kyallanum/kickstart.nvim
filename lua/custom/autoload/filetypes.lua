83d5ef9ab4d3d97631483d48c1989740f8873bb3
Kyal Lanum <kyal.lanum@mend.io>
2024-04-23 21:43:31 -0600

    Updated plugins with JSON lua. Removed local schema association. Adjusted some JSON settings

--- lua/custom/autoload/filetypes.lua
+++ lua/custom/autoload/filetypes.lua
@@ -2,3 +2,21 @@ vim.api.nvim_create_autocmd({ 'BufEnter', 'BufNewFile' }, {
   pattern = '.whitesource',
   command = 'set filetype=json tabstop=2',
 })
+
+vim.api.nvim_create_autocmd({ 'BufWinEnter', 'BufNewFile' }, {
+  callback = function(opts)
+    if vim.bo[opts.buf].filetype == 'json' then
+      vim.cmd 'set shiftwidth=2'
+      vim.cmd 'set foldmethod=manual'
+      vim.cmd 'silent! loadview'
+    end
+  end,
+})
+
+vim.api.nvim_create_autocmd({ 'BufWinLeave' }, {
+  callback = function(opts)
+    if vim.bo[opts.buf].filetype == 'json' then
+      vim.cmd 'mkview'
+    end
+  end,
+})
